036b4b9df27c4077a92f073255e694a4f5df65c7
Umaidahmad01 <ahmedumaid707@gmail.com>
2024-07-21 02:23:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 OWNER_ID = os.getenv("OWNER_ID",5585016974)
-HANDLER = ["~", ".","!","?","@","$"]
+HANDLER = ["5585016974", "5585016974","!","?","@","$"]
 BARATH = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
 GROUP_ID = os.getenv("GROUP_ID", "")
 SOURCE = ""
